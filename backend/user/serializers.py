ff13b28e059adb4e6441e4c30de886bac9d1c67f
Amr <amrmohamed.amaa@gmail.com>
2024-11-23 19:26:22 +0200

    fix backend

--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import User
 
+
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, min_length=8)
     confirm_password = serializers.CharField(write_only=True, min_length=8)
@@ -8,10 +9,7 @@ class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = (
-            'username',
             'email',
-            'first_name',
-            'last_name',
             'title',
             'bio',
             'phone_number',
@@ -24,7 +22,7 @@ class RegisterSerializer(serializers.ModelSerializer):
         if data['password'] != data['confirm_password']:
             raise serializers.ValidationError("Passwords do not match.")
         return data
-    
+
     def create(self, validated_data):
         user = User(
             username=validated_data['username'],
--- backend/user/views.py
+++ backend/user/views.py
@@ -1,10 +1,11 @@
 from django.db.models import Max
-from user_account.serializers import RegisterSerializer
-from user_account.models import User
+from user.serializers import RegisterSerializer
+from user.models import User
 from rest_framework.response import Response
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
 from rest_framework.views import APIView
+
 
 class UserCreateAPIView(generics.CreateAPIView):
     queryset = User.objects.all()
