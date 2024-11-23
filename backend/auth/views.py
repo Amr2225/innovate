0c5bcf52d869d74a8f1defbd6ecdc0f34f6001b4
Mohamed Mokhtar <mohammed.waleed@citystars.com.eg>
2024-11-23 18:01:22 +0200

    auth code

--- backend/auth/views.py
+++ backend/auth/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.db.models import Max
+from api.serializers import RegisterSerializer
+from api.models import User
+from rest_framework.response import Response
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
+from rest_framework.views import APIView
 
-# Create your views here.
+class UserCreateAPIView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
