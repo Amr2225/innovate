7c86ccda3b72dcd6fb6b9b795bc926013348027b
mohammedwaleed04 <90641420+mohammedwaleed04@users.noreply.github.com>
2025-01-15 14:00:56 +0200

    Add files via upload

---
+++ backend/institution/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.InstitutionListCreateAPIView.as_view()),
+    path('<int:p_id>', views.RetrieveUpdateDestroyInstitutionDetailAPIView.as_view())
+]
