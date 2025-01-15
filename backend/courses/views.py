97d4b23d583744bb18ad2cf5ba97aa4c384e7f88
mohammedwaleed04 <90641420+mohammedwaleed04@users.noreply.github.com>
2025-01-15 14:00:01 +0200

    Add files via upload

---
+++ backend/courses/views.py
@@ -0,0 +1,26 @@
+from courses.serializers import CourseSerializer
+from courses.models import Course
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
+from rest_framework.views import APIView
+
+
+class CourseListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+    def get_permissions(self):
+        self.permission_classes = [AllowAny]
+        if self.request.method == 'POST':
+            self.permission_classes = [AllowAny]
+        return super().get_permissions()
+
+
+class RetrieveUpdateDestroyCourseDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+    lookup_url_kwarg = 'p_id'
+    def get_permissions(self):
+        self.permission_classes = [AllowAny]
+        if self.request.method in ['PUT', 'PATCH', 'DELETE']:
+            self.permission_classes = [IsAuthenticated]
+        return super().get_permissions()
