6f542802b70d17eea3b99da18864814234abb9c0
mohammedwaleed04 <90641420+mohammedwaleed04@users.noreply.github.com>
2025-04-15 23:06:53 +0200

    Delete backend directory

--- backend/courses/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.CourseListCreateAPIView.as_view()),
-    path('<int:p_id>', views.RetrieveUpdateDestroyCourseDetailAPIView.as_view())
-]
--- backend/users/validation.py
+++
@@ -1,9 +0,0 @@
-from django.core.exceptions import ValidationError
-
-
-def nationalId_length_validation(value):
-    nationalId = value
-    nationalIdString = str(nationalId)
-    nationalIdLength = len(nationalIdString)
-    if nationalIdLength < 14 or nationalIdLength > 14:
-        raise ValidationError("Nationalid ID is invalid")
