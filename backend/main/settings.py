1784469793c27f82a89ff2c438d9e7d84202378f
Amr <amrmohamed.amaa@gmail.com>
2025-05-11 12:49:29 +0300

    attempt 3 fix environment variables refrence

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -8,7 +8,7 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DEBUG = os.environ.get("DEBUG", default=0)
 
