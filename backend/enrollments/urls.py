9cac5839a408b857dbec0be81ade90ecce718f6c
Mohamed Mokhtar <mohammed.waleed@citystars.com.eg>
2025-06-11 00:44:50 +0300

    faculty student promotion

--- backend/enrollments/urls.py
+++ backend/enrollments/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('enroll', views.EligibleCoursesAPIView.as_view(), name='courses-eligible'),
     path('my-courses', views.EnrolledCoursesAPIView.as_view(), name='my-enrolled-courses'),
+    path('<uuid:enrollment_id>/assessment-scores/', views.EnrollmentAssessmentScoresView.as_view(), name='enrollment-assessment-scores'),
+    path('<uuid:enrollment_id>/score/', views.EnrollmentScoreView.as_view(), name='enrollment-score'),
+    path('<uuid:pk>/update-score/', views.EnrollmentUpdateScoreView.as_view(), name='enrollment-update-score'),
+    path('promote-students/faculty', views.PromoteStudentsFacultyAPIView.as_view(), name='promote-students-semester')
 ]
--- backend/institution_policy/models.py
+++ backend/institution_policy/models.py
@@ -6,7 +6,7 @@ import uuid
 class InstitutionPolicy(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     institution = models.OneToOneField(User, on_delete=models.CASCADE, related_name='policy', limit_choices_to={"role": "Institution"})
-    min_passing_grade = models.FloatField(default=50.0, null=True, blank=True)
+    min_passing_percentage = models.FloatField(default=50.0, null=True, blank=True)
     max_allowed_failures = models.IntegerField(default=2, null=True, blank=True)
     min_gpa_required = models.FloatField(null=True, blank=True)
     min_attendance_percent = models.FloatField(default=75.0, null=True, blank=True)
